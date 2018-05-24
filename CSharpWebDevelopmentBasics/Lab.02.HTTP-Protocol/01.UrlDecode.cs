f854ae76d2a0684ed6e5baa6223f37a9918c38b5
Hristo-R <hristo.razpopov@abv.bg>
2018-05-24 16:27:42 +0300

    added Lab.02.HTTP-Protocol/01.UrlDecode.cs

---
+++ CSharpWebDevelopmentBasics/Lab.02.HTTP-Protocol/01.UrlDecode.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.UrlDecode
+{
+    using System;
+    using System.Net;
+
+    public class UrlDecode
+    {
+        public static void Main()
+        {
+            Console.Write("input: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"output: {WebUtility.UrlDecode(input)}");
+        }
+    }
+}
