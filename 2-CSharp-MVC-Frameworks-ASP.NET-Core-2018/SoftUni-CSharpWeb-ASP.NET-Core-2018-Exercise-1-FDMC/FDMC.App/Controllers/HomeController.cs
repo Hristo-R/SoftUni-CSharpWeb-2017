fd60b6e6b5ad434305c2b5931823a6096420277d
Hristo-R <hristo.razpopov@abv.bg>
2018-09-01 18:41:27 +0300

    upload

---
+++ 2-CSharp-MVC-Frameworks-ASP.NET-Core-2018/SoftUni-CSharpWeb-ASP.NET-Core-2018-Exercise-1-FDMC/FDMC.App/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿namespace FDMC.App.Controllers
+{
+    using FDMC.App.Models.ViewModels;
+    using FDMC.Data;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+
+    public class HomeController : Controller
+    {
+        public HomeController(CatContext context)
+        {
+            this.Context = context;
+        }
+
+        public CatContext Context { get; set; }
+
+        public IActionResult Index()
+        {
+            var cats = this.Context
+                .Cats
+                .Select(cat => new CatConciseViewModel()
+                {
+                    Id = cat.Id,
+                    Name = cat.Name
+                })
+                .ToArray();
+
+            return View(model: cats);
+        }
+    }
+}
---
+++ 2-CSharp-MVC-Frameworks-ASP.NET-Core-2018/SoftUni-CSharpWeb-ASP.NET-Core-2018-Exercise-1-FDMC/FDMC.Data/CatContext.cs
@@ -0,0 +1,21 @@
+﻿namespace FDMC.Data
+{
+    using FDMC.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CatContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Database=Cats_MVC;Integrated Security=True");
+                //optionsBuilder.UseSqlServer("Data Source=MS-PC\\SQLEXPRESS;Database=Cats_MVC;Integrated Security=True");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
